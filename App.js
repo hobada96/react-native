78d120955b93da71b40913510044f2680d73d9d5
hobada96 <hobada96@gmail.com>
2018-01-30 17:15:13 +0900

    This is Yeshow version 1

--- App.js
+++ App.js
@@ -1,52 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput,} from 'react-native';
+import {View, Text, styles, StyleSheet, backgroundColor, borderWidth, marginTop, textAlign, fontsize} from 'react-native';
 
-class WeatherProject extends Component {
-    constructor(props){
-        super(props);
-        // zip 코드의 기본 값을 지정하고 싶으면 다음 코드를 수정한다.
-        this.state = { zip: ''};
-    }
-    //이 콜백을 <TexztInput>에 지정
-    _handleTextChange(event) {
-        //Xcode나 리모트 JS 디버그 콘솔에 로그가 보여진다.
-        console.log(event.nativeEvent.text);
-        this.setState({
-            zip: event.nativeEvent.txt
-        });
-    }
-    render() {
+export class extends Component {
+    render(){
         return(
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                You input {this.state.zip}.
-                </Text>
-                <TextInput
-                    style={styles.input}
-                    returnKeyType='go'
-                    onSubitEditing={(event) => this._handleTextChange(event)}/>
+            <View style={styles.parent}>
+                <Text style={styles.child}> Child one </Text>
+                <Text style={styles.child}> Child Two </Text>
+                <Text style={styles.child}> Child Three </Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#F5FCFF',
-        },
-        welcome: {
-            fontSize: 20,
-            textAlign: 'center',
-            margin: 10,
-        },
-        input: {
-            fontSize: 20,
-            borderWidth: 2,
-            height: 40
-        }
+    parent: {
+        backgroundColor: '#F5FCFF',
+//        borderColor: '#0099AA',
+        borderWidth: 5,
+        marginTop: 30
+    },
+    child: {
+        borderColor: '#AA0099',
+        borderWidth: 2,
+//        textAlign: 'Center',
+        fontSize: 24,
+    }
 });
-
-export default WeatherProject;
